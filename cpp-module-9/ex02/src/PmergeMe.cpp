140ce615594c964dd95efbe454929ffbd90aaaf8
Mateo Pizzolo <mpizzolo@c1r1s3.42madrid.com>
2023-09-20 10:15:33 +0200

    adding comments

--- cpp-module-9/ex02/src/PmergeMe.cpp
+++ cpp-module-9/ex02/src/PmergeMe.cpp
@@ -17,37 +17,57 @@ double PmergeMe::numsToContainers(std::string nums)
 	return res;
 }
 
+
 template <typename Iterator>
 void insertionSort(Iterator begin, Iterator end)
 {
+	// Loop through the elements in the range.
 	for (Iterator i = begin; i != end; ++i)
 	{
+		// Store the current element in the 'key' variable.
 		typename Iterator::value_type key = *i;
+		
+		// Initialize a pointer 'j' to the current element.
 		Iterator j = i;
+		
+		// While 'j' is not at the beginning of the range and 'key' is less than the previous element.
 		while (j != begin)
 		{
+			// Create a pointer 'prev' to the previous element.
 			Iterator prev = j;
 			--prev;
+			
+			// If 'key' is less than the previous element, swap them.
 			if (key < *prev)
 			{
 				std::swap(*j, *prev);
 			}
 			else
 			{
+				// If 'key' is greater or equal to the previous element, break the loop.
 				break;
 			}
+			
+			// Move 'j' one position to the left.
 			--j;
 		}
 	}
 }
 
+// This is a function to merge two sorted ranges defined by iterators.
 template <typename Iterator>
 void merge(Iterator begin, Iterator mid, Iterator end)
 {
+	// Create a temporary vector to store the merged result.
 	std::vector<typename Iterator::value_type> temp(std::distance(begin, end));
+	
+	// Initialize pointers 'left' and 'right' to the beginning of the two sub-ranges.
 	Iterator left = begin, right = mid;
+	
+	// Initialize an iterator 'tempIt' to the beginning of the temporary vector.
 	typename std::vector<typename Iterator::value_type>::iterator tempIt = temp.begin();
-
+	
+	// Merge the two sub-ranges into the temporary vector.
 	while (left != mid && right != end)
 	{
 		if (*left <= *right)
@@ -62,25 +82,32 @@ void merge(Iterator begin, Iterator mid, Iterator end)
 		}
 		++tempIt;
 	}
-
+	
+	// Copy any remaining elements from the left and right sub-ranges.
 	tempIt = std::copy(left, mid, tempIt);
 	std::copy(right, end, tempIt);
+	
+	// Copy the merged result back to the original range 'begin' to 'end'.
 	std::copy(temp.begin(), temp.end(), begin);
 }
 
 template <typename Iterator>
 void mergeInsertionSort(Iterator begin, Iterator end, int threshold)
 {
+	// If the range size is small enough, use insertion sort.
 	if (std::distance(begin, end) <= threshold)
 	{
 		insertionSort(begin, end);
 	}
 	else
 	{
+		// Otherwise, split the range in half and recursively apply mergeInsertionSort.
 		Iterator mid = begin;
 		std::advance(mid, std::distance(begin, end) / 2);
 		mergeInsertionSort(begin, mid, threshold);
 		mergeInsertionSort(mid, end, threshold);
+		
+		// Merge the two sorted sub-ranges.
 		merge(begin, mid, end);
 	}
 }
