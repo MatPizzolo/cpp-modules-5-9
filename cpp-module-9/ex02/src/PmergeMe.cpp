21bb84e35f178080c9031cf7e2cdaaf42b1d40de
Mateo Pizzolo <mpizzolo@c2r16s1.42madrid.com>
2023-09-24 16:08:58 +0200

    arreglando el merge sort

--- cpp-module-9/ex02/src/PmergeMe.cpp
+++ cpp-module-9/ex02/src/PmergeMe.cpp
@@ -5,138 +5,109 @@ PmergeMe::~PmergeMe() {}
 
 double PmergeMe::numsToContainers(std::string nums)
 {
-	std::istringstream iss(nums);
-	std::string token;
+    std::istringstream iss(nums);
+    std::string token;
 
-	while (iss >> token)
-	{
-		_myList.push_back(std::atoi(token.c_str()));
-		_myVec.push_back(std::atoi(token.c_str()));
-	}
-	double res = _myVec.size();
-	return res;
+    while (iss >> token)
+    {
+        _myList.push_back(std::atoi(token.c_str()));
+        _myVec.push_back(std::atoi(token.c_str()));
+    }
+    double res = _myVec.size();
+    return res;
 }
 
-
-template <typename Iterator>
-void insertionSort(Iterator begin, Iterator end)
-{
-	// Loop through the elements in the range.
-	for (Iterator i = begin; i != end; ++i)
-	{
-		// Store the current element in the 'key' variable.
-		typename Iterator::value_type key = *i;
-		
-		// Initialize a pointer 'j' to the current element.
-		Iterator j = i;
-		
-		// While 'j' is not at the beginning of the range and 'key' is less than the previous element.
-		while (j != begin)
-		{
-			// Create a pointer 'prev' to the previous element.
-			Iterator prev = j;
-			--prev;
-			
-			// If 'key' is less than the previous element, swap them.
-			if (key < *prev)
-			{
-				std::swap(*j, *prev);
-			}
-			else
-			{
-				// If 'key' is greater or equal to the previous element, break the loop.
-				break;
-			}
-			
-			// Move 'j' one position to the left.
-			--j;
-		}
-	}
+// Function to perform binary search and find the insertion position
+int binarySearch(const std::vector<int>& S, int element) {
+    int left = 0;
+    int right = S.size() - 1;
+    while (left <= right) {
+        int mid = (left + right) / 2;
+        if (S[mid] == element) {
+            return mid;  // Element already exists in S
+        } else if (S[mid] < element) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return left;  // Insert element at the left position
 }
 
-// This is a function to merge two sorted ranges defined by iterators.
-template <typename Iterator>
-void merge(Iterator begin, Iterator mid, Iterator end)
-{
-	// Create a temporary vector to store the merged result.
-	std::vector<typename Iterator::value_type> temp(std::distance(begin, end));
-	
-	// Initialize pointers 'left' and 'right' to the beginning of the two sub-ranges.
-	Iterator left = begin, right = mid;
-	
-	// Initialize an iterator 'tempIt' to the beginning of the temporary vector.
-	typename std::vector<typename Iterator::value_type>::iterator tempIt = temp.begin();
-	
-	// Merge the two sub-ranges into the temporary vector.
-	while (left != mid && right != end)
-	{
-		if (*left <= *right)
-		{
-			*tempIt = *left;
-			++left;
-		}
-		else
-		{
-			*tempIt = *right;
-			++right;
-		}
-		++tempIt;
-	}
-	
-	// Copy any remaining elements from the left and right sub-ranges.
-	tempIt = std::copy(left, mid, tempIt);
-	std::copy(right, end, tempIt);
-	
-	// Copy the merged result back to the original range 'begin' to 'end'.
-	std::copy(temp.begin(), temp.end(), begin);
-}
+// Function to perform Merge-Insertion Sort
+std::vector<int> mergeInsertionSort(const std::vector<int>& X) {
+    int n = X.size();
+    if (n <= 1) {
+        return X;  // Base case: already sorted or empty vector
+    }
 
-template <typename Iterator>
-void mergeInsertionSort(Iterator begin, Iterator end, int threshold)
-{
-	// If the range size is small enough, use insertion sort.
-	if (std::distance(begin, end) <= threshold)
-	{
-		insertionSort(begin, end);
-	}
-	else
-	{
-		// Otherwise, split the range in half and recursively apply mergeInsertionSort.
-		Iterator mid = begin;
-		std::advance(mid, std::distance(begin, end) / 2);
-		mergeInsertionSort(begin, mid, threshold);
-		mergeInsertionSort(mid, end, threshold);
-		
-		// Merge the two sorted sub-ranges.
-		merge(begin, mid, end);
-	}
+    // Pairing
+    std::vector<int> largerElements;
+    std::vector<std::vector<int> > pairs;
+    for (int i = 0; i < n; i += 2) {
+        std::vector<int> pair;
+        if (i + 1 < n) {
+            pair.push_back(X[i]);
+            pair.push_back(X[i + 1]);
+        } else {
+            pair.push_back(X[i]);
+        }
+        pairs.push_back(pair);
+    }
+
+    // Comparison and Sorting
+    for (size_t i = 0; i < pairs.size(); ++i) {
+        int largerElement = std::max(pairs[i][0], pairs[i][1]);
+        largerElements.push_back(largerElement);
+    }
+
+    // Recursion
+    std::vector<int> sortedLargerElements = mergeInsertionSort(largerElements);
+
+    // Insertion
+    std::vector<int> S;
+    int firstElementOfS = sortedLargerElements[0];
+    for (int i = 0; i < n; ++i) {
+        if (X[i] != firstElementOfS) {
+            int insertionPosition = binarySearch(S, X[i]);
+            S.insert(S.begin() + insertionPosition, X[i]);
+        }
+    }
+    
+    // Insert the first element (smallest) of the larger elements
+    S.insert(S.begin(), firstElementOfS);
+    
+    return S;
 }
 
 double PmergeMe::sortVec()
 {
-	clock_t start = clock();
-	mergeInsertionSort(_myVec.begin(), _myVec.end(), 5);
-	clock_t end = clock();
-	double elapsedTime = static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000000;
-	return (elapsedTime);
+    clock_t start = clock();
+    // mergeInsertionSort(_myVec.begin(), _myVec.end(), 5);
+    _myVec = mergeInsertionSort(_myVec);
+    clock_t end = clock();
+    double elapsedTime = static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000000;
+    return (elapsedTime);
 }
 
 double PmergeMe::sortList()
 {
-	clock_t start = clock();
-	mergeInsertionSort(_myList.begin(), _myList.end(), 5);
-	clock_t end = clock();
-	double elapsedTime = static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000000;
-	return (elapsedTime);
+    clock_t start = clock();
+    // mergeInsertionSort(_myList.begin(), _myList.end(), 5);
+    clock_t end = clock();
+    double elapsedTime = static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000000;
+    return (elapsedTime);
 }
 
-PmergeMe::PmergeMe(PmergeMe& copy) {
-	this->_myList = copy._myList;
-	this->_myVec = copy._myVec;
+PmergeMe::PmergeMe(PmergeMe &copy)
+{
+    this->_myList = copy._myList;
+    this->_myVec = copy._myVec;
 }
 
-PmergeMe &PmergeMe::operator=(const PmergeMe& other) {
-	this->_myList = other._myList;
-	this->_myVec = other._myVec;
-	return *this;
+PmergeMe &PmergeMe::operator=(const PmergeMe &other)
+{
+    this->_myList = other._myList;
+    this->_myVec = other._myVec;
+    return *this;
 }
